a8c73863cce17bf8ff810f517004bada1210a2a5
aneesh199608 <aneesh199608@gmail.com>
2017-07-23 11:56:03 +0530

    Create firstsql.sql

---
+++ firstsql.sql
@@ -0,0 +1,20 @@
+drop database raveen;
+create database raveen;
+select database();
+use raveen;
+select database();
+create table hisfriends
+(
+    name varchar(15) not null,
+    age int,
+    sex varchar(1) not null
+    );
+    describe hisfriends;
+    explain hisfriends;
+create table aneesh
+(
+    name varchar(15) not null,
+    age int,
+    sex varchar(1) not null
+    );
+show databases;
